8f726c1c55ddc7411c3a07322f239da1601abbbb
Djaenk <wanganthony.wang@gmail.com>
2021-04-21 04:18:45 -0500

    Set up scheduling of listing updates

--- backend/scraper.js
+++ backend/scraper.js
@@ -1,5 +1,14 @@
-import { consoles } from './consoles.js';
 import puppeteer from 'puppeteer';
+
+const consoles = require('consoles');
+const cron = require('node-cron');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "consolestock",
+    password: "cs4345",
+    database: "consolestock"
+});
 
 const selectors = {
 	Amazon: {
@@ -31,21 +40,30 @@ export const listingScraper = {
 		}
 	},
 
-	async scrapeListing(system, marketplace){
+	async terminate(){
+		if(this.browser){
+			await this.browser.close();
+		}
+	},
+
+	async scrapeListing(system, marketplace, uri){
 		this.system = system;
 		this.marketplace = marketplace;
-		const uri = consoles.get(system).get(marketplace);
+		if(!uri){
+			uri = consoles.get(system).get(marketplace);
+		}
 		await this.loadPage(uri);
 		
 		let listing = {
-			type: system,
-			seller: marketplace,
-			name: await this.getTitle(),
-			price: await this.getPrice(),
+			title: await this.getTitle(),
+			brand: brand,
+			type: type,
 			condition: "New",
-			active: await this.getAvailability(),
+			seller: marketplace,
 			url: uri.split(' ')[0],
+			active: await this.getAvailability(),
 			date: this.generateTimestamp(),
+			price: await this.getPrice(),
 		};
 		return listing;
 	},
@@ -94,10 +112,33 @@ export const listingScraper = {
 	}
 };
 
-(async () => {
+async function addListings(){
 	await listingScraper.init();
-	console.log(await listingScraper.scrapeListing("PlayStation 5", "Amazon"));
-	console.log(await listingScraper.scrapeListing("Switch", "BestBuy"));
-	console.log(await listingScraper.scrapeListing("Xbox Series X", "Newegg"));
-	listingScraper.browser.close();
-})();
+	consoles.consoles.forEach((value, system) => {
+		value.forEach((uri, seller) => {
+			const listing = await listingScraper.scrapeListing(key, seller);
+			console.log(listing);
+			// INSERT query
+		})
+	})
+	await listingScraper.terminate();
+}
+
+async function updateListings(){
+	await listingScraper.init();
+	connection.query("SELECT console_type_name, console_seller_name, console_listing_url FROM console_listings AS l INNER JOIN console_types AS t ON t.console_type_id=l.console_type_id INNER JOIN console_sellers AS s ON s.console_seller_id=l.console_seller_id;", async (error, results, fields) => {
+		if (error) {
+			console.error(error);
+		}
+		for (var i of results){
+			const listing = await listingScraper.scrapeListing(results[i].console_type_name, results[i].console_seller_name, results[i].console_listing_url);
+			console.log(listing);
+			// UPDATE query
+		}
+	})
+	await listingScraper.terminate()
+}
+
+cron.schedule('0 */6 * * *', async () => {
+	await updateListings();
+});
